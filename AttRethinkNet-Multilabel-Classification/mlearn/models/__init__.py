f3bc274e332a83114678d32df5d19cfcf8ca8242
RanSuLab <44308471+RanSuLab@users.noreply.github.com>
2021-12-21 16:39:48 +0800

    Add files via upload

---
+++ AttRethinkNet-Multilabel-Classification/mlearn/models/__init__.py
@@ -0,0 +1,9 @@
+# from .binary_relevance import BinaryRelevance
+# from .classifier_chains import ClassifierChains
+# from .csrpe import CSRPE
+# from .probabilistic_classifier_chains import ProbabilisticClassifierChains
+# from .random_k_labelsets import RandomKLabelsets
+from .rethinknet.rethinkNet import RethinkNet
+
+__all__ = ['RethinkNet']
+# __all__ = ['CSRPE', 'RethinkNet']
